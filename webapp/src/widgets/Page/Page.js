a2d3b56e120bd7e833f6ae2771b8da4e2f2b621e
Santosh Rawat <santosh.rawat@vicesoftware.com>
2020-06-08 21:05:53 +0530

    minor changes

--- webapp/src/widgets/Page/Page.js
+++ webapp/src/widgets/Page/Page.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import './page.css'
-import { Container } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
 
 export default function Page({ children }) {
-	return <Container fluid>{children}</Container>
+	return (
+			<Container fluid className='bg-light'>
+				{children}
+			</Container>
+		)
 }
