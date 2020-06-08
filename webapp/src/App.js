351317bb330af428252e8bdb5b41d8182eeca563
Santosh Rawat <santosh.rawat@vicesoftware.com>
2020-06-08 17:48:06 +0530

    added react datepicker

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import NavBar from './widgets/NavBar'
 import Routes from './Routes'
+import NotificationPopup from './infrastructure/notificationPopup'
 import './app.css'
 
 const App = () => {
 	return (
 		<Router>
+			<NavBar />
+			<NotificationPopup />
 			<Routes />
 		</Router>
 	)
--- webapp/src/features/dashboard/DashboardFilter.js
+++ webapp/src/features/dashboard/DashboardFilter.js
@@ -1,41 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Button, Form } from 'react-bootstrap'
 import Cards from '../../widgets/cards'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import './DashboardFilter.css'
 
 const DashboardFilter = () => {
+	const [startDate, setStartDate] = useState(
+		new Date().setMonth(new Date().getMonth() - 1)
+	)
+	const [endDate, setEndDate] = useState(new Date())
+
 	return (
 		<Col className='pb-25' lg={4} xl={3}>
 			<Cards title='Filter'>
 				<Form.Group className='mb-15'>
-					<Form.Label>STARTING DATE</Form.Label>
-					<Form.Control
-						className='calender-input'
-						type='text'
-						name=''
-						placeholder=''
-						value='APRIL 1, 2020'
+					<Form.Label>start date</Form.Label>
+					<DatePicker
+						selected={startDate}
+						onChange={(startDate) => setStartDate(startDate)}
+						dateFormat='MMMM d, yyyy'
+						className='form-control calender-input'
 					/>
 				</Form.Group>
 				<Form.Group className='mb-15'>
-					<Form.Label>END DATE</Form.Label>
-					<Form.Control
-						className='calender-input'
-						type='text'
-						name=''
-						placeholder=''
-						value='APRIL 30, 2020'
+					<Form.Label>end date</Form.Label>
+					<DatePicker
+						selected={endDate}
+						onChange={(endDate) => setEndDate(endDate)}
+						dateFormat='MMMM d, yyyy'
+						className='form-control calender-input'
 					/>
 				</Form.Group>
 				<Form.Group className='mb-15'>
-					<Form.Label>PLAN</Form.Label>
+					<Form.Label>plan</Form.Label>
 					<Form.Control as='select'>
-						<option>All PLANS</option>
+						<option>all plans</option>
 					</Form.Control>
 				</Form.Group>
 				<Form.Group className='mb-15'>
-					<Form.Label>SUBSCRIPTION STATUS</Form.Label>
+					<Form.Label>subscription status</Form.Label>
 					<Form.Control as='select'>
-						<option>All STATUS</option>
+						<option>all status</option>
 					</Form.Control>
 				</Form.Group>
 				<Button variant='secondary'>Update</Button>
